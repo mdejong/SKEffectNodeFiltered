b3966461920d4070e1a00597980955dca4e72522
Mo DeJong <mo@modejong.com>
2019-01-29 15:53:49 -0800

    initial import of empty project

---
+++ SKEffectNodeFiltered/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  SKEffectNodeFiltered
+//
+//  Created by Mo DeJong on 1/29/19.
+//  Copyright © 2019 HelpURock. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ SKEffectNodeFiltered/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  SKEffectNodeFiltered
+//
+//  Created by Mo DeJong on 1/29/19.
+//  Copyright © 2019 HelpURock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
